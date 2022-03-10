cc6071a37c978b8266497c809ed3ed68ca0fd679
alexsyy <76884549+alexsyy@users.noreply.github.com>
2022-03-10 17:08:39 +0100

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Ariane</title>
+</head>
+
+<body>
+<h1>
+    <a href="/ariane/login.htm">Connexion</a>
+    <hr/>
+    <a href="/ariane/newuser.htm">Nouvel utilisateur</a>
+    <hr/>
+    <a href="/ariane/admin.php">Administration</a>
+</h1>
+</body>
+
+</html>
