31366458d1fd6735c8e1368530651d15ae0cd7c3
alexsyy <alexsyypro@outlook.fr>
2022-03-11 16:53:14 +0100

    Initial

--- index.php
+++ index.php
@@ -2,20 +2,13 @@
 <html>
 
 <head>
-    <title>Ariane</title>
+    <title>FoneMarket</title>
 </head>
 
 <body>
 <h1>
-    TEST GITHUB
+    FoneMarket
 </h1>
-<h2>
-    arwind EFFECTUE UN TEST 
-    j'ai faim
-</h2>
-<h3>
-    NICE ARWIND NIIIIIIIIIIIIIIICE zqdddzfeyttrytr qqzd qzd qzd qzd qzd qzd qzd qzd qzd qdd qjkbhgihohdhffiuiudauidaefayfeuaehfioheazfdff
-</h3>
 </body>
 
 </html>
