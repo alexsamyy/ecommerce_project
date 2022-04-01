9e725f052e30ee4c8e9b1b9927884859f061b079
alexsyy <alexsyypro@outlook.fr>
2022-04-01 15:09:24 +0200

    index debug

--- index.php
+++ index.php
@@ -2,42 +2,18 @@
 <html>
 
 <head>
-<<<<<<< HEAD
-    <title>FoneMarket</title>
-=======
     <meta charset="utf-8">
     <title>FoneMarket Index</title>
->>>>>>> back-end
 </head>
 
 <body>
 <h1>
-<<<<<<< HEAD
-    FoneMarket
-=======
     <a href="login.php">Connexion</a>
     <hr/>
     <a href="newuser.php">Nouvel utilisateur</a>
     <hr/>
     <a href="home.php">Home</a>
->>>>>>> back-end
 </h1>
-<<<<<<< HEAD
-<h2>
-    arwind EFFECTUE UN TEST 
-    j'ai faim
-</h2>
-<h3>
-     c'était sur en fait
-</h3>
-<h4> 
-    boom boom le bureau
-</h4>
-<h5>
-    YEAGER
-</h5>
-=======
->>>>>>> 31366458d1fd6735c8e1368530651d15ae0cd7c3
 </body>
 <footer>
     
