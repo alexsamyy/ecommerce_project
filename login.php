a5e91fbff9f5e8b043b48c3a598a9b7d6066abc8
alexsyy <alexsyypro@outlook.fr>
2022-04-04 12:17:13 +0200

    debug barre de recherche (modification fichier style.css)

--- login.php
+++ login.php
@@ -11,7 +11,7 @@
         <div id="container">
             <!-- zone de connexion -->
             
-            <form action="verification_login.php" method="POST">
+            <form id="login" action="verification_login.php" method="POST">
                 <h1>Connexion</h1>
 
                 <label><b>E-mail</b></label><br/>
